272861c8aa93acea4dedb400f7003843df048880
Pratikvc10 <66675803+Pratikvc10@users.noreply.github.com>
2025-09-30 18:57:32 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('calendar-cache').then(cache => {
+    caches.open('calendar-cache-v4').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/manifest.json'
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
       ]);
     })
   );
@@ -12,8 +14,6 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
